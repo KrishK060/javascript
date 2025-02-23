f151a2b666389557ac85a6ffacc3063959aff42b
KrishK060 <kalariakrish0601@gmail.com>
2025-02-23 23:41:34 +0530

    modifies callback folder

--- callback.js
+++ callback.js
@@ -11,17 +11,28 @@
 //     getdata(2);
 // });
 
-const getPromise=()=>{
-    return new Promise((resolve,reject)=>{
-        console.log("i am");
-        resolve("sucess")
-    })
-};
+// const getPromise=()=>{
+//     return new Promise((resolve,reject)=>{
+//         console.log("i am");
+//         resolve("sucess")
+//     })
+// };
 
-let promise = getPromise();
-promise.then((res)=>{
-    console.log("promise fullfilled",res);
-});
-promise.catch((err)=>{
-    console.log("rejected",err);
-});
+// let promise = getPromise();
+// promise.then((res)=>{
+//     console.log("promise fullfilled",res);
+// });
+// promise.catch((err)=>{
+//     console.log("rejected",err);
+// });
+function myDisplayer(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+  
+  function myCalculator(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+  }
+  
+  let result = myCalculator(5, 5);
+  myDisplayer(result);
